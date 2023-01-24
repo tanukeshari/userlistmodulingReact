383ef770db9bbd5ce17b374f1cded59c85a0f1e6
tanukeshari <kesharitanu@gmail.com>
2023-01-24 18:21:44 +0530

    React.createPortal-

--- src/Components/UI/ErrorModel.js
+++ src/Components/UI/ErrorModel.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModel.module.css";
+import ReactDOM from 'react-dom';
 
-const ErrorModel =props =>{
-    return(<div>
-        <div className={classes.backdrop } onClick={props.onConfirm}>
-            <Card className={classes.model}>
+const Backdrop =(props)=>{
+    return <div className={classes.backdrop} onClick={props.onConfirm}/>
+};
+
+const ModelOverlay =(props) =>{
+return(
+    <Card className={classes.model}>
             <header className={classes.hearder}>
                 <h2>{props.title}</h2>
             </header>
@@ -14,11 +18,28 @@ const ErrorModel =props =>{
                 <p>{props.message}</p>
         </div>
         <footer className={classes.action}>
-            <Button onClick={props.onClick}>okey</Button>
+            <Button onClick={props.onConfirm}>okey</Button>
         </footer>
         </Card>
-    </div>
-   </div> 
+
+
+)
+}
+
+const ErrorModel =props =>{
+    return(
+    <React.Fragment>
+        {ReactDOM.createPortal(<Backdrop onConfirm ={props.onConfirm} />, document.getElementById('backdrop-root')
+        )}
+        {ReactDOM.createPortal(
+        <ModelOverlay 
+          title ={props.title}
+          message= {props.message}
+          onConfirm={props.onConfirm}
+        />,
+     document.getElementById('overlay-root')
+        )}
+        </React.Fragment> 
    );
 };
 export default ErrorModel;
